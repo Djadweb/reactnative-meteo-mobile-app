fbcc627778919c2dc608c628c6f69908a9648dd0
djamel <djadweb@gmail.com>
2021-12-25 18:02:40 +0100

    frontend & api setup

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,18 +1,37 @@
 import React, { useState } from 'react'
-import { View, Text, ImageBackground, StyleSheet, SafeAreaView, StatusBar, TextInput } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, SafeAreaView, StatusBar, TextInput, Button } from 'react-native'
+import axios from 'axios'
 import cold from '../../assets/cold.jpg'
 
 const Home = () => {
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState();
+
+    const getWeather = () => {
+        const api = "fb4c68b2c921418c658c71e0b4bb9286";
+        try {
+            const res = axios.get(`api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${api}`)
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }        
+    }
     return (
         <ImageBackground source={cold} style={styles.cold}>
-            <SafeAreaView style={styles.container}>                            
-                <TextInput                 
-                    style={styles.cityInput}
-                    value={city}
-                    placeholder='اكتب مدينتك'
-                    onChangeText={text => setCity(text)}
-                />                
+            <SafeAreaView style={styles.container}>
+                <View style={styles.city}>
+                    <TextInput                 
+                        style={styles.cityInput}
+                        value={city}
+                        placeholder='اكتب مدينتك'
+                        onChangeText={text => setCity(text)}
+                    />                             
+                    <Button
+                    onPress={getWeather}
+                        style={styles.validateBtn}                    
+                        title="Ok"
+                        color="#000"                        
+                    />
+                </View>                                            
             </SafeAreaView>                    
         </ImageBackground>
     )
@@ -20,8 +39,8 @@ const Home = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,  
-        alignItems: 'center',
+        flex: 1,          
+        alignItems: 'flex-start',          
     },
     cold: {
         flex: 1,
@@ -29,6 +48,15 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%'                       
     },  
+    city: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 50,        
+        width: '100%',       
+        maxHeight: 100,                         
+    },
     cityInput: {
         backgroundColor: "rgba(255, 255, 255, .7)",
         fontWeight: "bold",
@@ -38,6 +66,10 @@ const styles = StyleSheet.create({
         height: 60,
         marginVertical: 50,
         borderRadius: 10,
+    },
+    validateBtn: {
+        width: 150,
+        height: 50
     }
 })
 
